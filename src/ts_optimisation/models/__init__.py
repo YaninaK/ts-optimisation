0d73635c283851879a8c245394761dbd71c4e655
YaninaK <kutovaiayp@yahoo.com>
2024-12-05 22:24:42 +0300

    Add process_model.py

--- src/ts_optimisation/models/__init__.py
+++ src/ts_optimisation/models/__init__.py
@@ -1,1 +1,11 @@
+MODEL_CONFIG = {
+    "input_sequence_length": 4,
+    "n_features": 12,
+    "stat_units_max": 32,
+    "stat_units_min": 16,
+    "lstm_n_units_max": 32,
+    "lstm_n_units_min": 16,
+}
+
+
 OUTPUT_FILE_NAME = "output.txt"
---
+++ src/ts_optimisation/models/process_model.py
@@ -0,0 +1,84 @@
+import logging
+from typing import Optional
+
+import pandas as pd
+import tensorflow as tf
+
+logger = logging.getLogger(__name__)
+
+__all__ = ["define_process_model"]
+
+from . import MODEL_CONFIG
+
+
+def get_model(df_stat: pd.DataFrame, config: Optional[dict] = None) -> tf.keras.Model:
+    """
+    Генерирует модель, работающую с последовательностями (LSTM) и
+    статическими признаками для фиксации закономерностей процесса флотации.
+
+    """
+    if config is None:
+        config = MODEL_CONFIG
+
+    norm_Ni_Feed_mom = tf.keras.layers.Normalization(axis=None, name="norm_Ni_Feed_mom")
+    norm_Ni_Feed_mom.adapt(df_stat["Ni_Feed_mom"].fillna(0).values)
+
+    norm_Cu_Feed_mom = tf.keras.layers.Normalization(axis=None, name="norm_Cu_Feed_mom")
+    norm_Cu_Feed_mom.adapt(df_stat["Cu_Feed_mom"].fillna(0).values)
+
+    norm_Density_Feed_mom = tf.keras.layers.Normalization(
+        axis=None, name="norm_Density_Feed_mom"
+    )
+    norm_Density_Feed_mom.adapt(df_stat["Density_Feed_mom"].fillna(0).values)
+
+    inputs = {
+        "Ni_Feed_mom": tf.keras.Input(shape=(1,), dtype=int, name=f"Ni_Feed_mom"),
+        "Cu_Feed_mom": tf.keras.Input(shape=(1,), dtype=float, name=f"Cu_Feed_mom"),
+        "Density_Feed_mom": tf.keras.Input(
+            shape=(1,), dtype=int, name=f"Density_Feed_mom"
+        ),
+        "LSTM input": tf.keras.Input(
+            shape=(config["input_sequence_length"], config["n_features"]),
+            name="LSTM input",
+        ),
+    }
+    layers = []
+    layers.append(norm_Ni_Feed_mom(inputs["Ni_Feed_mom"]))
+    layers.append(norm_Cu_Feed_mom(inputs["Cu_Feed_mom"]))
+    layers.append(norm_Density_Feed_mom(inputs["Density_Feed_mom"]))
+
+    stat_features = tf.keras.layers.Concatenate(axis=-1, name="stat_features")(layers)
+
+    X_stat = tf.keras.layers.Dense(
+        config["stat_units_max"], activation="relu", name="dense_1"
+    )(stat_features)
+    X_stat = tf.keras.layers.Dense(
+        config["stat_units_min"], activation="relu", name="dense_2"
+    )(X_stat)
+
+    encoder_output1, state_h1, state_c1 = tf.keras.layers.LSTM(
+        config["lstm_n_units_max"],
+        return_sequences=True,
+        return_state=True,
+        name="encoder_output1",
+    )(inputs["LSTM input"])
+    encoder_states1 = [state_h1, state_c1]
+
+    encoder_output2 = tf.keras.layers.LSTM(
+        config["lstm_n_units_min"], return_state=False, name="encoder_output2"
+    )(encoder_output1)
+
+    concatenated = tf.keras.layers.Concatenate(axis=-1, name="all_features")(
+        [encoder_output2, X_stat]
+    )
+    X_all = tf.keras.layers.Dense(
+        config["stat_units_min"], activation="relu", name="dense_3"
+    )(concatenated)
+
+    outputs = tf.keras.layers.Dense(
+        config["n_features"], activation="linear", name="dense_4"
+    )(X_all)
+
+    model = tf.keras.Model(inputs, outputs)
+
+    return model
