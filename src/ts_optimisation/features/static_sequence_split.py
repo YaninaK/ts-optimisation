0d4964c8c3ff9f6e9459d900a92f2714c89ea7f6
YaninaK <kutovaiayp@yahoo.com>
2024-12-06 11:09:18 +0300

    Add static_sequence_split.py

---
+++ src/ts_optimisation/features/static_sequence_split.py
@@ -0,0 +1,47 @@
+import os
+import sys
+
+sys.path.append(os.getcwd())
+sys.path.append(os.path.join(os.getcwd(), "src", "ts_optimisation"))
+
+import logging
+from typing import Optional, Tuple
+
+import numpy as np
+import pandas as pd
+from data import DATA_CONFIG as CONFIG
+
+logger = logging.getLogger(__name__)
+
+__all__ = ["generate_static_and_sequence_datasets"]
+
+
+def generate_static_and_sequence_datasets(
+    df: pd.DataFrame,
+    config: Optional[dict] = None,
+) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Генерирует 2 датасета:
+        df_stat - на датасет со статичными данными
+        df_seq - датасет с временными рядами.
+    """
+    if config is None:
+        config = CONFIG
+
+    df_stat = (
+        df[config["input_stat_feature_names"]]
+        .reset_index()
+        .groupby("fm_id", as_index=False)
+        .agg(
+            n_periods=("time", "count"),
+            Ni_Feed_mom=("Ni_Feed_mom", "first"),
+            Cu_Feed_mom=("Cu_Feed_mom", "first"),
+            Density_Feed_mom=("Density_Feed_mom", "first"),
+            Rec_Conc_mom=("Rec_Conc_mom", "first"),
+        )
+    )
+    df_seq = df[
+        [col for col in df.columns if col not in config["input_stat_feature_names"]]
+    ]
+
+    return df_stat, df_seq
